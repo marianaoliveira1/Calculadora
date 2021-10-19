5066471ed731db21f90c25bfa1ceaeb9cb64da87
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-19 19:22:30 -0300

    up

--- lib/components/button_row.dart
+++ lib/components/button_row.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
 
-class Button extends StatelessWidget {
-  const Button({Key? key}) : super(key: key);
+class ButtonRow extends StatelessWidget {
+
+  final List<Button> buttons;
+
+  ButtonRow(this.buttons);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons,
+      ),
+    );
   }
 }
--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
+import 'button_row.dart';
 
 class Keyboard extends StatelessWidget {
   const Keyboard({Key? key}) : super(key: key);
@@ -7,6 +9,38 @@ class Keyboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
+      child: Column(
+        children: [
+          ButtonRow([
+            Button(text: 'AC', big: true),
+            Button(text: '%'),
+            Button.operation(text: '/'),
+          ]),
+          ButtonRow([
+            Button(text: '7'),
+            Button(text: '8'),
+            Button(text: '9'),
+            Button.operation(text: 'x'),
+          ]),
+          ButtonRow([
+            Button(text: '4'),
+            Button(text: '5'),
+            Button(text: '6'),
+            Button.operation(text: '-'),
+          ]),
+          ButtonRow([
+            Button(text: '1'),
+            Button(text: '2'),
+            Button(text: '3'),
+            Button.operation(text: '+'),
+          ]),
+          ButtonRow([
+            Button(text: '0', big: true),
+            Button(text: '.'),
+            Button.operation(text: '='),
+          ]),
+        ],
+      ),
     );
   }
 }
