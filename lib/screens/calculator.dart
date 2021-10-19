4bd5c28c2290cc22247fc77ba409c649add5623e
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-19 08:38:19 -0300

    up

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/display.dart';
+import '../components/keyboard.dart';
 
 class Calculator extends StatelessWidget {
   const Calculator({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class Calculator extends StatelessWidget {
       home: Column(
         children: [
           Display('123654'),
-          Text('Keyboard'),
+          Keyboard(),
         ],
       ),
     );
